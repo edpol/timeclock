72ad63e3050e3fe392f7153711edb80b4c40fe33
Ed Pol <edpol@p4bh.org>
2017-07-03 10:00:39 -0400

    Removed ACL

---
+++ timeclock/connect.php
@@ -0,0 +1,7 @@
+<?php
+/* connect to the database used by timeclock */
+defined('DB_SERVER')    ? null : define("DB_SERVER",    "localhost");
+defined('DB_USER')      ? null : define("DB_USER",      "timemaster");
+defined('DB_PASS')      ? null : define("DB_PASS",      "svte7!vw");
+defined('DB_NAME')      ? null : define("DB_NAME",      "mdr_clock");
+?>
